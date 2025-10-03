097ea871285de35590b169cc44dca787a22e98ba
Andrey Sliva <andreysliva@Andreys-MacBook-Air-2.local>
2025-10-03 11:11:01 +0200

    (feat): init form no validation

---
+++ apps/frontend/hr_front/src/App.tsx
@@ -0,0 +1,12 @@
+import Form from "./components/Form"
+
+function App() {
+
+  return (
+    <div className='w-full h-screen flex justify-center items-center bg-yellow-200'>
+        <Form />
+    </div>
+  )
+}
+
+export default App
