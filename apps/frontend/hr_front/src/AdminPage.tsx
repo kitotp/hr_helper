f88f136cbe41c2837df671bf632fe8ec0344358b
Andrey Sliva <andreysliva@Andreys-MacBook-Air-2.local>
2025-10-04 17:43:22 +0200

    front/admin_login_form no validation

---
+++ apps/frontend/hr_front/src/AdminPage.tsx
@@ -0,0 +1,7 @@
+export default function AdminPage(){
+    return(
+        <div className="bg-yellow-200 h-screen flex fex-col items-center">
+            <p>Admin</p>
+        </div>
+    )
+}
--- apps/frontend/hr_front/src/main.tsx
+++ apps/frontend/hr_front/src/main.tsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
+import AdminLoginForm from './AdminLoginForm'
+import AdminPage from './AdminPage'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/admin',
+    element: <AdminLoginForm />
+  },
+  {
+    path: '/admin/dashboard',
+    element: <AdminPage />
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
