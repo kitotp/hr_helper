eb905d56db9cfe4ea5cf2e3cf9d33ef7ed9dccc9
Andrey Sliva <andreysliva@Andreys-MacBook-Air-2.local>
2025-10-05 14:56:59 +0200

    (feat): context API for api

---
+++ apps/frontend/hr_front/src/components/ApplicationCard.tsx
@@ -0,0 +1,18 @@
+import type { Application } from "../AdminPage"
+
+type Props = {
+    card: Application
+}
+
+export default function ApplicationCard({card}: Props){
+
+    return(
+        <div className="border border-black h-[60px] flex flex-row justify-between px-2">
+            <div className="flex flex-col items-start justify-center">
+                <h1>{card.name}</h1>
+                <p>{card.created_at}</p>
+            </div>
+            <button className="border border-black bg-purple-400 text-white font-semibold px-2 py-1 ">Analize</button>
+        </div>
+    )
+}
