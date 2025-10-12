c19c3051ceb194ca0e13ecc44995b929a128e713
Andrey Sliva <andreysliva@Andreys-MacBook-Air-2.local>
2025-10-12 19:24:32 +0200

    (feat): admin page design changed

--- apps/frontend/hr_front/src/AdminPage.tsx
+++ apps/frontend/hr_front/src/AdminPage.tsx
@@ -97,32 +97,38 @@ export default function AdminPage(){
     
 
     return(
-        <div className="bg-yellow-200 h-screen flex flex-row items-center justify-center gap-8">
-            <div className="w-[500px] h-[800px] bg-white border border-black py-3 px-3 overflow-y-auto">
-                <h1 className="text-black font-semibold text-[24px]">Applications List of {admin?.username}</h1>
-                <div className="flex flex-col justify-center gap-2">
-                    {applications.map((application: Application) => (<ApplicationCard key={application.id} card={application}/>))}
+        <div className="h-screen w-full flex flex-col items-center bg-gray-100 justify-center md:px-50 md:py-20">
+            <div className="bg-white w-full h-full px-5 py-5 flex flex-col items-center">
+                <h1 className="text-3xl font-semibold self-start">Hiring Dashboard</h1>
+                <p className="w-full h-[1px] bg-gray-300"></p>
+                <div className="h-fit w-fit rounded-2xl flex flex-row mt-5 items-center justify-center gap-8">
+                    <div className="w-[500px] h-[700px] rounded-2xl bg-white border border-gray-300 py-3 px-3 overflow-y-auto">
+                        <h1 className="text-black font-semibold text-[24px]">Applications List of {admin?.username}</h1>
+                        <div className="flex flex-col justify-center gap-2">
+                            {applications.map((application: Application) => (<ApplicationCard key={application.id} card={application}/>))}
+                        </div>
+                    </div>
+                    <div className="w-[500px] h-[700px] rounded-2xl border border-gray-300 py-3 px-3">
+                        <h1 className="text-black font-semibold text-[24px]">Settings</h1>
+
+                        <form onSubmit={handleSubmit(submitForm)} className="flex flex-col">
+                            <input type="text" placeholder="Email" className="border border-black rounded-xl py-1 px-2" {...register('email')}/>
+                            <input type="text" placeholder="Company name" className="border border-black rounded-xl py-1 mt-2 px-2" {...register('company_name')}/>
+                            <h1 className="text-2xl font-bold mt-5">AI Agent analizing parameters</h1>
+                            <h2>Job you are looking for</h2>
+                            <input placeholder="Ex. Frontend Engineer" type="text" className="border rounded-xl border-black p-2" {...register('job')}/>
+
+                            <h2>Experience</h2>
+                            <input type="text" placeholder="Ex. 2 years" className="border border-black rounded-xl p-2" {...register('experience')}/>
+
+                            <h2>Stack you are looking for</h2>
+                            <textarea placeholder="Ex. React, TypeScript, Redux ToolKit" className="border rounded-xl border-black p-2" {...register('stack')}/>
+
+                            <button type="submit" className="border mt-5 self-center border-black py-3 px-8 text-white rounded-lg bg-purple-500">Save</button>
+                        </form>
+
+                    </div>
                 </div>
-            </div>
-            <div className="w-[500px] h-[800px] border border-black py-3 px-3">
-                <h1 className="text-black font-semibold text-[24px]">Settings</h1>
-
-                <form onSubmit={handleSubmit(submitForm)} className="flex flex-col">
-                    <input type="text" placeholder="Email" className="border border-black rounded-xl py-1 px-2" {...register('email')}/>
-                    <input type="text" placeholder="Company name" className="border border-black rounded-xl py-1 mt-2 px-2" {...register('company_name')}/>
-                    <h1 className="text-2xl font-bold mt-5">AI Agent analizing parameters</h1>
-                    <h2>Job you are looking for</h2>
-                    <input placeholder="Ex. Frontend Engineer" type="text" className="border border-black p-2" {...register('job')}/>
-
-                    <h2>Experience</h2>
-                    <input type="text" placeholder="Ex. 2 years" className="border border-black p-2" {...register('experience')}/>
-
-                    <h2>Stack you are looking for</h2>
-                    <textarea placeholder="Ex. React, TypeScript, Redux ToolKit" className="border border-black p-2" {...register('stack')}/>
-
-                    <button type="submit" className="border mt-5 self-center border-black py-3 px-8 text-white rounded-lg bg-purple-500">Save</button>
-                </form>
-
             </div>
         </div>
     )
